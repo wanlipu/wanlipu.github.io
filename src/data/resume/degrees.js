0bb8b654e082c00bc085ece1d50c32a74952c281
wanlipu <18710247+wanlipu@users.noreply>
2023-03-03 21:17:49 -0800

    update resume

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,7 +3,11 @@ const degrees = [
     school: 'Georgia Institute of Technolog',
     degree: 'M.S. Computer Science',
     link: 'https://www.gatech.edu/',
-    year: 2020,
+  },
+  {
+    school: 'Colorado School of Mines',
+    degree: 'M.S. Petroleum Engineering',
+    link: 'https://www.mines.edu/',
   },
 ];
 
--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -5,9 +5,10 @@ const positions = [
     link: 'https://www.aboutamazon.com/',
     daterange: 'March 2021 - Present',
     points: [
-      'Designed and implemented cloud based services for Automated Alexa Benchmarking',
-      'Designed and implemented tools that improve operational excellence',
-      'Led business intelligence effort to deliver Alexa Benchmarking metric reports',
+      'Designed and implemented cloud-based services to facilitate the automated benchmarking of Alexa, the virtual assistant developed by Amazon',
+      'Drove efforts to deliver accurate and actionable insights into the performance of Alexa, and conducted research and analysis on best practices in metrics onboarding and user adoption',
+      'Designed and implemented tools that improve operational excellence within our organization,  these tools were focused on improving the efficiency, reliability, and quality of our day-to-day operations',
+      'Deployed NLP models in a scalable way with AWS, and implemented cost reduction measures in the process',
     ],
   },
   {
@@ -16,8 +17,8 @@ const positions = [
     link: 'http://www.michaels.com/',
     daterange: 'October 2020 - March 2021',
     points: [
-      'Conducted user interviews to collect requirements for inventory management system',
-      'Designed ERD and created schema for inventory management system',
+      'Gathered requirements for an inventory management system by conducting user interviews',
+      'Developed the schema and Entity Relationship Diagram (ERD) for an inventory management system',
     ],
   },
   {
@@ -26,7 +27,19 @@ const positions = [
     link: 'https://www.datanovainc.com/',
     daterange: 'March 2018 - October 2020',
     points: [
-      'Worked on various oil and gas related projects',
+      'Developed a Python package for optimizing water-flooding processes that was validated through a field pilot test and subsequently scaled up for field-wide operations',
+      'Designed and implemented data processing and machine learning pipelines for automatically processing input files and tuning models to optimize water-flooding processes, and employed parallel computing techniques during the process',
+      'Applied gradient-boosting algorithms to predict results from water plugging profile simulations, which reduced the time needed for water plugging design optimization by using machine learning models instead of simulations',
+    ],
+  },
+  {
+    company: 'Datanova Analytics Inc',
+    position: 'Data Scientist',
+    link: 'https://www.pantoninc.com/',
+    daterange: 'July 2016 - February 2018',
+    points: [
+      'Applied image processing and machine learning techniques to analyze aerial images in order to identify potential roof damage on buildings. We were able to quickly and accurately detect areas of concern, such as missing shingles, cracks, and leaks',
+      'Possessed a Part 107 Commercial Drone Pilot License and performed field operations. With this license, I was able to operate unmanned aerial vehicles (UAVs) commercially',
     ],
   },
 ];
